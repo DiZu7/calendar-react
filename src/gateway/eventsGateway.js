e0ceb1a7c26c4c16a99ce5926fd025644c3ba7ad
DiZu7 <dianadovhanyuk@gmail.com>
2023-04-12 13:01:38 +0300

    updated gateway handling errors

--- src/gateway/eventsGateway.js
+++ src/gateway/eventsGateway.js
@@ -1,12 +1,4 @@
-const baseUrl = 'https://63949f6986829c49e8225bf8.mockapi.io/api/v1/events';
-
-const handleClientError = response => {
-  if (!response.ok) {
-    alert("Error. Can't display events");
-    return [];
-  }
-  return response.json();
-};
+const baseUrl = 'https://63949f6986829c49e8225bf8.mockapi.io/api/v1/event';
 
 const handleNetworkError = () => {
   alert("Network Error. Can't display events");
@@ -14,7 +6,15 @@ const handleNetworkError = () => {
 };
 
 export const fetchEventsList = () => {
-  return fetch(baseUrl).then(handleClientError).catch(handleNetworkError);
+  return fetch(baseUrl)
+    .then(response => {
+      if (!response.ok) {
+        alert("Error. Can't display events");
+        return [];
+      }
+      return response.json();
+    })
+    .catch(handleNetworkError);
 };
 
 export const createEvent = eventData => {
@@ -25,7 +25,13 @@ export const createEvent = eventData => {
     },
     body: JSON.stringify(eventData),
   })
-    .then(handleClientError)
+    .then(response => {
+      if (!response.ok) {
+        alert("Error. Can't create events");
+        return [];
+      }
+      return response.json();
+    })
     .catch(handleNetworkError);
 };
 
@@ -33,6 +39,12 @@ export const deleteEvent = eventId => {
   return fetch(`${baseUrl}/${eventId}`, {
     method: 'DELETE',
   })
-    .then(handleClientError)
+    .then(response => {
+      if (!response.ok) {
+        alert("Error. Can't delete events");
+        return [];
+      }
+      return response.json();
+    })
     .catch(handleNetworkError);
 };
